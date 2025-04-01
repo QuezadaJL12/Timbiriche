31eed325c3e62383aa8e4a09d79fc29d2ec8e308
RubenRamR <160176918+RubenRamR@users.noreply.github.com>
2025-03-31 17:54:34 -0700

    🚑

--- Timbiriche-GUI/src/main/java/mvcRegistro/VistaRegistro.java
+++ Timbiriche-GUI/src/main/java/mvcRegistro/VistaRegistro.java
@@ -35,9 +35,9 @@ public class VistaRegistro extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
         JPaneRegistro = new javax.swing.JPanel();
         txtNombre = new javax.swing.JTextField();
-        jLabel1 = new javax.swing.JLabel();
         panelAvatares = new javax.swing.JPanel();
         btnRegistrar = new javax.swing.JButton();
         LblFondoReg = new javax.swing.JLabel();
@@ -45,7 +45,21 @@ public class VistaRegistro extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        jLabel1.setBackground(new java.awt.Color(0, 0, 0));
+        jLabel1.setFont(new java.awt.Font("Comic Sans MS", 1, 48)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel1.setText("REGISTRO");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(840, 110, 260, -1));
+
+        txtNombre.setBackground(new java.awt.Color(225, 225, 225));
+        txtNombre.setFont(new java.awt.Font("Comic Sans MS", 0, 18)); // NOI18N
+        txtNombre.setForeground(new java.awt.Color(0, 0, 0));
         txtNombre.setText("Nick");
+        txtNombre.setToolTipText("Nick");
+        txtNombre.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Nick", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Comic Sans MS", 0, 12), new java.awt.Color(0, 0, 0))); // NOI18N
+        txtNombre.setDisabledTextColor(new java.awt.Color(204, 255, 255));
+        txtNombre.setName("Nick"); // NOI18N
+        txtNombre.setOpaque(true);
         txtNombre.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txtNombreActionPerformed(evt);
@@ -56,23 +70,22 @@ public class VistaRegistro extends javax.swing.JFrame {
         JPaneRegistro.setLayout(JPaneRegistroLayout);
         JPaneRegistroLayout.setHorizontalGroup(
             JPaneRegistroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(JPaneRegistroLayout.createSequentialGroup()
-                .addGap(71, 71, 71)
-                .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 258, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(91, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, JPaneRegistroLayout.createSequentialGroup()
+                .addContainerGap(80, Short.MAX_VALUE)
+                .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 265, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(75, 75, 75))
         );
         JPaneRegistroLayout.setVerticalGroup(
             JPaneRegistroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(JPaneRegistroLayout.createSequentialGroup()
-                .addGap(90, 90, 90)
+                .addGap(93, 93, 93)
                 .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(128, Short.MAX_VALUE))
+                .addContainerGap(96, Short.MAX_VALUE))
         );
 
-        getContentPane().add(JPaneRegistro, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 100, 420, 240));
+        txtNombre.getAccessibleContext().setAccessibleName("Nick");
 
-        jLabel1.setText("REGISTRO");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 70, -1, -1));
+        getContentPane().add(JPaneRegistro, new org.netbeans.lib.awtextra.AbsoluteConstraints(760, 220, 420, 240));
 
         javax.swing.GroupLayout panelAvataresLayout = new javax.swing.GroupLayout(panelAvatares);
         panelAvatares.setLayout(panelAvataresLayout);
@@ -85,18 +98,22 @@ public class VistaRegistro extends javax.swing.JFrame {
             .addGap(0, 250, Short.MAX_VALUE)
         );
 
-        getContentPane().add(panelAvatares, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 360, 400, 250));
+        getContentPane().add(panelAvatares, new org.netbeans.lib.awtextra.AbsoluteConstraints(770, 500, 400, 250));
 
+        btnRegistrar.setBackground(new java.awt.Color(255, 255, 255));
+        btnRegistrar.setFont(new java.awt.Font("Comic Sans MS", 1, 24)); // NOI18N
+        btnRegistrar.setForeground(new java.awt.Color(0, 0, 0));
         btnRegistrar.setText("Registrarse");
+        btnRegistrar.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
         btnRegistrar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnRegistrarActionPerformed(evt);
             }
         });
-        getContentPane().add(btnRegistrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 640, 210, 50));
+        getContentPane().add(btnRegistrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(860, 780, 220, 50));
 
         LblFondoReg.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/FondoReg1.1.png"))); // NOI18N
-        getContentPane().add(LblFondoReg, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 30, -1, -1));
+        getContentPane().add(LblFondoReg, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
